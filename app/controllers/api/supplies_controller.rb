351e4f83cfa291cdbd20244c38fa62c6d733913b
Mark <markandrewhamm@gmail.com>
2020-07-10 12:05:14 -0500

    wrote validations

--- app/controllers/api/supplies_controller.rb
+++ app/controllers/api/supplies_controller.rb
@@ -1,5 +1,7 @@
 class Api::SuppliesController < ApplicationController
 
+  
+
   def index
     @supplies = Supply.all
     render 'index.json.jb'
@@ -8,5 +10,34 @@ class Api::SuppliesController < ApplicationController
   def show
     @supply = Supply.find_by(id: params[:id])
     render 'show.json.jb'
+  end
+
+  def create
+    @supply = Supply.new(
+      name: params[:name],
+      department: params[:department],
+      price: params[:price]
+    )
+    if @supply.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @supply.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @supply = Supply.find_by(id: params[:id])
+    @supply.update(
+      name: params[:name] || @supply.name,
+      department: params[:department] || @supply.department,
+      price: params[:price] || @supply.price
+    )
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @supply = Supply.find_by(id: params[:id])
+    @supply.destroy
+    render :json => {message: "The item has been deleted"}
   end
 end
--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,2 +1,8 @@
 class Supply < ApplicationRecord
+
+  validates :name, presence: true, length: {in: 1..50}
+  validates :department, presence: true, inclusion: { :in => ["sleeping", "hyrdation", "shelters"]}
+  validates :price, presence: true, numericality: { greater_than: 0 }, numericality: { less_than: 100000 }
+  
 end
+
